4a59429a0829d5dde7044fea2399a9b967078bce
Callisto <Callisto>
2023-03-23 00:53:50 +0100

    Switched to LCDSerializer protocol

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -8,7 +8,7 @@
 #define LED_PIN 10		// This is the pin the backlight is controlled by, it must be a PWM pin
 #define STARTUP_BRIGHTNESS 2  // What backlight brightness to start up with (50% by default).
 
-# define BAUDRATE 57600  // What baudrate to use for the serial port
+# define BAUDRATE 57600 // What baudrate to use for the serial port
 
 // There was a reason I did not use these as defines, but I can't
 // remember it :-/
@@ -26,6 +26,9 @@ const int LCDH = 4;
 	It seems the library actually supports both 8-bit and 4-bit mode. It attempts
 	8-bit, then falls back to 4-bit */
 LiquidCrystal lcd(12, 11, 2, 3, 4, 5, 6, 7, 8, 9);
+
+
+byte cmd; //will hold our sent command
 
 
 void set_backlight(int value) {
@@ -50,23 +53,38 @@ void setup() {
 
 }
 
-byte serial_read() {
+int serial_read() {
 	int result = -1;
 	while(result == -1) {
 		if(Serial.available() > 0) {
 			result = Serial.read();
 		}
 	}
-	return (byte)result;
+	return result;
 }
 
+/**
+ * From : https://github.com/lcdproc/lcdproc/blob/master/server/drivers.c
+ * All the serial connections supported by driver
+ *
+
+static const struct hd44780_SerialInterface serial_interfaces[] = {
+//        type                  instr ms  data     v     ^   pre bitrate bits  K   esc   cmd  B  Besc  Boff   Bon Multi  End
+	{ HD44780_CT_PICANLCD,      0x11,  0, 0x12, 0x00, 0x1F,    0,   9600,   8, 0, 0x00,    0, 0,    0,    0,    0,   0,    0 },
+	{ HD44780_CT_LCDSERIALIZER, 0xFE,  0,    0, 0x00, 0x00,    0,   9600,   8, 0, 0x00,    0, 0,    0,    0,    0,   0,    0 },
+	{ HD44780_CT_LOS_PANEL,     0xFE,  0,    0, 0x00, 0x00,    0,   9600,   4, 1, 0xFE,    0, 1, 0xFD,    0, 0xFF,   0,    0 },
+	{ HD44780_CT_VDR_LCD,       0xFE,  0,    0, 0x00, 0x00,    0,   9600,   4, 0, 0x00,    0, 0,    0,    0,    0,   0,    0 },
+	{ HD44780_CT_VDR_WAKEUP,    0xC0,  0, 0xC4, 0xC0, 0xCF,    0,   9600,   4, 0, 0x00,    0, 1,    0, 0xC9, 0xC8,   1, 0xCF },
+	{ HD44780_CT_PERTELIAN,     0xFE,  0,    0, 0x00, 0x00,    0,   9600,   8, 0, 0x00,    0, 1, 0xFE, 0x02, 0x03,   0,    0 },
+	{ HD44780_CT_EZIO,          0xFE, 40,    0, 0x00, 0x00, 0x28,   2400,   4, 1, 0xFD, 0x06, 0,    0,    0,    0,   0,    0 }
+};
+
+*/
 
 void loop() {
-	byte cmd; //will hold our sent command
 	cmd = serial_read();
 	switch(cmd) {
-			case 0x11:
-				// Write LCD instruction -- used by lcdproc driver
+			case 0xFE:
 				lcd.command(serial_read());
 				break;
 			default:
